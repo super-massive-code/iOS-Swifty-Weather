730d94425d8c2ad750d2311ea9d4f15c13d060de
super-massive-code <101387594+super-massive-code@users.noreply.github.com>
2023-07-10 14:53:44 +0100

    init hello world with .gitignore

---
+++ swifty_weather/swifty_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_weatherApp.swift
+//  swifty_weather
+//
+//  Created by Carl Taylor on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct swifty_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
